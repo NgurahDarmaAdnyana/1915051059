dc51dfc4caf7f6deab734d1b06d09f5ee4c4e44f
NgurahDarmaAdnyana <71536846+NgurahDarmaAdnyana@users.noreply.github.com>
2020-11-01 13:16:01 +0800

    Add files via upload

---
+++ admin/views/joki/tambah.php
@@ -0,0 +1,30 @@
+<h4>Tambah Data</h4>
+<hr>
+<form action="index.php?mod=joki&page=save"method="POST">
+	<div class="col-md-6">
+	<div class="form group">
+		<label for="">ID</label>
+		<input type="text" name="id" required value="<?=(isset($_POST['id']))?$_POST['id']:'';?>" class="form-control"> 
+		 <span class="text-danger"><?=(isset($err['id']))?$err['id']:'';?></span>
+	</div>
+	<div class="form-group">
+        <label for="">Nama</label>
+        <input type="text" name="nama" required value="<?=(isset($_POST['nama']))?$_POST['nama']:'';?>" class="form-control">
+		 <span class="text-danger"><?=(isset($err['nama']))?$err['nama']:'';?></span>
+        </div>
+	<div class="form group">
+		<label for="">Jasa</label>
+		<input type="text" name="jasa" required value="<?=(isset($_POST['jasa']))?$_POST['jasa']:'';?>" class="form-control" id="">
+		 <span class="text-danger"><?=(isset($err['jasa']))?$err['jasa']:'';?></span>
+	</div>	
+<div class="form-group">
+<label for="">Photo</label>
+<img src="../media/<?=$_POST['photo']?>" width="60">
+ <input type="file" name="fileToUpload" class="form-control">
+ <span class="text-danger"><?=(isset($err['fileToUpload']))?$err['fileToUpload']:'';?></span>
+</div>		
+	 <div class="form-group">
+            <button type="submit" class="btn btn-primary">Save</button>
+    </div>
+    </div>
+</form>
